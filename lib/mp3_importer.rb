473d8c50cc9b3092942b235fe658902a79e37fa5
Eli Huebner <elimhuebner@gmail.com>
2020-09-22 14:45:12 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+  attr_reader :path
+  
+  @files = []
+  
+  def initialize(path)
+    @path = path
+    @files = Dir.children(@path)
+  end
+  
+  def files
+    @files
+  end
+  
+  def import
+    self.files.each {|file| Song.new_by_filename(file)}
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name=(name)
+    Artist.find_or_create_by_name(name).add_song(self)
+  end
+  
+  def self.new_by_filename(filename)
+    parsed_name = filename.split(" - ")
+    new_song = self.new(parsed_name[1])
+    new_song.artist_name = parsed_name[0]
+    new_song
+  end
+  
+end
