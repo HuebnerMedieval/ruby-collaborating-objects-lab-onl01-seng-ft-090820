7a3634c02e378cbc86113180e8549c84ce942882
Eli Huebner <elimhuebner@gmail.com>
2020-09-22 14:38:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,14 @@
 class MP3Importer
   attr_reader :path
   
+  @files = []
+  
   def initialize(path)
     @path = path
   end
   
   def self.files
-    
+    @files
   end
   
   def self.import
